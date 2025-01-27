c9f38a811476a3c7b1bb2810900155f73ba566e3
Anshuman Mishra <gamerzp780@gmail.com>
2025-01-27 13:16:56 +0000

    Created more css file added navbar and sidebar to all the pages fixed some issues

--- js/script.js
+++ js/script.js
@@ -1,1 +1,8 @@
+document.addEventListener('DOMContentLoaded', function () {
+    document.querySelector('#loader').style.display = 'none';
+  });
 
+  function toggleSidebar() {
+    const sidebar = document.getElementById("sidebar");
+    sidebar.classList.toggle("open");
+  }
