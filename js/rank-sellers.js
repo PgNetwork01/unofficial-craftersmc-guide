60962db6076e579da5b366eb64ba43beb32781e3
Techon ;- <148909669+TechonVS@users.noreply.github.com>
2025-02-01 21:05:56 +0000

    Try to reactivate the sidebar

--- js/rank-sellers.js
+++ js/rank-sellers.js
@@ -1,6 +1,11 @@
 document.addEventListener('DOMContentLoaded', function () {
   document.querySelector('#loader').style.display = 'none';
 });
+function toggleSidebar() {
+  const sidebar = document.getElementById("sidebar");
+  sidebar.classList.toggle("open");
+}
+
 
 let darkmode = localStorage.getItem ('darkmode' )
 const themeSwitch = document.getElementById( 'theme-switch')
