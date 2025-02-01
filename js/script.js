e71af41241ac31a0fde6e8376d40f6e8ad693956
Anshuman Mishra <gamerzp780@gmail.com>
2025-02-01 06:11:48 +0000

    added dark mode

--- js/script.js
+++ js/script.js
@@ -1,8 +1,50 @@
 document.addEventListener('DOMContentLoaded', function () {
-    document.querySelector('#loader').style.display = 'none';
-  });
+  document.querySelector('#loader').style.display = 'none';
+});
 
-  function toggleSidebar() {
-    const sidebar = document.getElementById("sidebar");
-    sidebar.classList.toggle("open");
-  }
+function toggleSidebar() {
+  const sidebar = document.getElementById("sidebar");
+  sidebar.classList.toggle("open");
+}
+
+// Toggle dark and light mode
+
+document.addEventListener("DOMContentLoaded", () => {
+  const sidebar = document.getElementById("sidebar");
+  const sidebarLink = document.querySelectorAll(".sidebar-link");
+  const close = document.querySelectorAll(".sidebar-close");
+  const bottomNav = document.querySelectorAll(".bottom-nav");
+  const bottomNavItem = document.querySelectorAll(".nav-item");
+  const bottomNavIcon = document.querySelectorAll(".botton-nav-icon");
+  const toggleButton = document.getElementById("darkModeToggle");
+  let isDarkMode = false;
+
+  toggleButton.addEventListener("click", () => {
+    if (isDarkMode) {
+      // Light Mode
+      document.body.style.backgroundColor = "white";
+      document.body.style.color = "black";
+      sidebar.style.backgroundColor = "white";
+      sidebar.style.color = "black";
+      bottomNav.forEach(nav => nav.style.backgroundColor = "white");
+      sidebarLink.forEach(link => link.style.color = "#000");
+      close.forEach(button => button.style.color = "black");
+      bottomNavItem.forEach(nav => nav.style.color = "#333333");
+      bottomNavIcon.forEach(nav => nav.style.color = "#333333");
+    } else {
+      // Dark Mode
+      document.body.style.backgroundColor = "#333333";
+      document.body.style.color = "white";
+      sidebar.style.backgroundColor = "#111111";
+      sidebar.style.color = "white";
+      bottomNav.forEach(nav => nav.style.backgroundColor = "#111111");
+      sidebarLink.forEach(link => link.style.color = "#fff");
+      close.forEach(button => button.style.color = "white");
+      bottomNavItem.forEach(nav => nav.style.color = "white");
+      bottomNavIcon.forEach(nav => nav.style.color = "white");
+    }
+    isDarkMode = !isDarkMode;
+  });
+});
+
+
