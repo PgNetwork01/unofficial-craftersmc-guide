47bc75d548c612425e9734fbe83c0145493dd82d
Techon ;- <148909669+TechonVS@users.noreply.github.com>
2025-02-01 18:54:42 +0000

    Fix smth

--- js/guides.js
+++ js/guides.js
@@ -1,8 +1,7 @@
 document.getElementById('dark-mode-toggle').addEventListener('click', (event) => {
   const button = event.target;
   const isDarkMode = document.body.classList.toggle('dark-mode');
-
-  button.textContent = isDarkMode ? '<span class="menu-icon material-icons" id="darkModeToggle">dark_mode</span>' : '☀️';
+  
   button.classList.toggle('dark-mode', isDarkMode);
   button.classList.toggle('light-mode', !isDarkMode);
 
