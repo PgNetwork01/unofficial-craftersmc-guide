6584b1b7e3344fa969762488b0aa11cca42d7051
Techon ;- <148909669+TechonVS@users.noreply.github.com>
2025-02-01 17:44:29 +0000

    Attemp 1

--- js/rank-sellers.js
+++ js/rank-sellers.js
@@ -2,12 +2,12 @@ let darkmode = localStorage.getItem ('darkmode' )
 const themeSwitch = document.getElementById( 'theme-switch')
 const enableDarkmode = () => {
   document.body. classList. add('darkmode')
-  localStorage setItem( 'darkmode', 'active')
+  localStorage.setItem( 'darkmode', 'active')
 }
 const disableDarkmode = () => {
   document. body. classList. remove ('darkmode')
   localStorage. setItem ('darkmode', null)
-｝
+}
 if(darkmode === "active") enableDarkmode ()
   themeSwitch.addEventListener ("click", () => {
   darkmode = localStorage.getItem( 'darkmode')
