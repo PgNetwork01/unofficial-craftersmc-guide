c521d6cec6cf746afb0471cd1745f0b550e67b87
Techon ;- <148909669+TechonVS@users.noreply.github.com>
2025-02-24 21:27:07 +0000

    J

--- js/tech-darkmode.js
+++ js/tech-darkmode.js
@@ -1,5 +1,5 @@
 let darkmode = localStorage.getItem ('darkmode' )
-const themeSwitch = document.getElementById( 'theme-switch')
+const themeSwitch = document.getElementById('theme-switch')
 const enableDarkmode = () => {
   document.body.classList.add('darkmode')
   localStorage.setItem('darkmode', 'active')
